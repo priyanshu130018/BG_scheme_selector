bde6f0f96476288fce79cc0f1a62a3134f739fe4
Priyanshu Ranjan Verma <13vermapriyanshu@gmail.com>
2025-10-30 02:51:14 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const changeBg= document.querySelectorAll(".box")
+const changeBg = document.querySelectorAll(".box")
 
-changeBg.forEach(box => {
-    box.addEventListener("click", (e) => {
+changeBg.forEach(i => {
+    i.addEventListener("click", (e) => {
         //console.log(e.target)
-        let indexColor=e.target.innerText.toLowerCase().trim();
-        document.body.style.backgroundColor=indexColor;
-
-        box.style.border=`3px solid ${indexColor}`;
+        let indexColor=e.target.innerText.toLowerCase().trim()
+        document.body.style.backgroundColor=indexColor
     })
 })
